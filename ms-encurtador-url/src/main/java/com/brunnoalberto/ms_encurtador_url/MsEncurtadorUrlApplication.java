b0cefee92055095bb092b5267b751078a83434d3
Brunno Alberto <brunnoalbertokr@gmail.com>
2026-06-07 14:30:25 -0300

    feat: subida do projeto spring

---
+++ ms-encurtador-url/src/main/java/com/brunnoalberto/ms_encurtador_url/MsEncurtadorUrlApplication.java
@@ -0,0 +1,13 @@
+package com.brunnoalberto.ms_encurtador_url;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEncurtadorUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEncurtadorUrlApplication.class, args);
+	}
+
+}
---
+++ ms-encurtador-url/src/test/java/com/brunnoalberto/ms_encurtador_url/MsEncurtadorUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunnoalberto.ms_encurtador_url;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEncurtadorUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
